d2cbded596ebde1f30fd9d25af90d05c7c598763
Andy Huynh <andy4thehuynh@gmail.com>
2014-12-15 15:53:29 -0800

    ALH: Created traffic command

--- main.go
+++ main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
+// Listen to status codes from unix commands.
+// 0 - Green.. it's done
+// 1 - Red.. something went wrong
+// other - Yellow.. still in progress
+
+// Change status bar color
+
 func main() {
-	fmt.Println("DUECES")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatalln("Not enough arguments")
+	}
+
+	cmd := exec.Command(args[0], args[1:]...)
+
+	data, err := cmd.Output()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Print(string(data))
 }
